566ec81c6ec3037a3815dcd9dea2643638335587
Luiz <luiz123o@hotmail.com>
2022-09-13 20:38:49 -0300

    test: add SignUpController return message error if no email provider

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,19 @@ describe('SignUpController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('missing param: name'))
   })
+
+  test('should return 400 if no email provider', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        password: 'valid_password',
+        confirmationPassword: 'valid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: email'))
+  })
 })
