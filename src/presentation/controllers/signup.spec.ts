e7de9b1edc4ba51a5f8e892c06d4c7cc407d25d3
Luiz Otavio Carioca Filho <luiz123o@hotmail.com>
2022-09-20 19:57:44 -0300

    test: ensure SignUpController returns 400 if an ivalid email is provider :white_check_mark:

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,13 +1,29 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { EmailValidator } from '../protocols/email-validator'
 import { SignUpController } from './signup'
 
-const makeSut = (): SignUpController => {
-  return new SignUpController()
+interface SUTTypes {
+  sut: SignUpController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SUTTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SignUpController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('SignUpController', () => {
   test('should return 400 if no name provider', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -22,7 +38,7 @@ describe('SignUpController', () => {
   })
 
   test('should return 400 if no email provider', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -37,7 +53,7 @@ describe('SignUpController', () => {
   })
 
   test('should return 400 if no password provider', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -52,7 +68,7 @@ describe('SignUpController', () => {
   })
 
   test('should return 400 if no confirmation password provider', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -66,5 +82,9 @@ describe('SignUpController', () => {
     expect(httpResponse.body).toEqual(
       new MissingParamError('confirmationPassword')
     )
+  })
+
+  test('should return 400 if an invalid email is provide', () => {
+
   })
 })
